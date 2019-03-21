5ec61a5f7e2b33ebf6e23332b4c81ba869397bd6
andrey.arakelyants <andrey.arakelyants@myoffice.team>
2019-03-21 11:26:20 +0300

    Shorten the code.

--- lesson_13_stack/ex_4_string_period.py
+++ lesson_13_stack/ex_4_string_period.py
@@ -3,18 +3,10 @@ from ex_1_z_function import get_z_function_effectively
 
 def get_min_string_period(string: str) -> str:
     z_function = get_z_function_effectively(string)
-    reversed_z_function = z_function[::-1]
-    period_index = 0
-    for index, prefix_length in enumerate(reversed_z_function):
-        initial_index = index
-        initial_prefix_length = prefix_length
-        while prefix_length == index + 1 and index < len(reversed_z_function):
-            index = initial_prefix_length + index
-            prefix_length = reversed_z_function[index]
-        if index == len(reversed_z_function) - 1:
-            period_index = len(string) - initial_index - 1
-            break
-    return string[period_index:]
+    last_index = len(string)
+    for index, prefix_length in enumerate(z_function):
+        if index + prefix_length == last_index and not last_index % index:
+            return string[:index]
 
 
 if __name__ == '__main__':
