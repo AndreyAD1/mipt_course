11c0df095cfa463087bce522e2ad4dd3ff034ef7
andrey.arakelyants <andrey.arakelyants@myoffice.team>
2019-04-15 19:04:18 +0300

    Add the ball control and air friction.

--- lesson_19_pygame_and_balls/ex_1.py
+++ lesson_19_pygame_and_balls/ex_1.py
@@ -12,18 +12,40 @@ x = 30
 y = 30
 vx = 50
 vy = 50
+BALL_RADIUS = 20
 
 while True:
     dt = clock.tick(50) / 1000.0
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+        if event.type == pygame.QUIT:
             sys.exit()
+
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        vy -= 5
+    elif pygame.key.get_pressed()[pygame.K_DOWN]:
+        vy += 5
+    elif pygame.key.get_pressed()[pygame.K_RIGHT]:
+        vx += 5
+    elif pygame.key.get_pressed()[pygame.K_LEFT]:
+        vx -= 5
+    else:
+        pass
+
+    vx -= 0.01 * vx
+    vy -= 0.01 * vy
 
     x += vx * dt
     y += vy * dt
 
-    border_reached = any([x < 0, x > width, y < 0, y > height])
+    border_reached = any(
+        [
+            x < BALL_RADIUS,
+            x > width - BALL_RADIUS,
+            y < BALL_RADIUS,
+            y > height - BALL_RADIUS
+        ]
+    )
     if border_reached:
         vx = -vx
         vy = -vy
