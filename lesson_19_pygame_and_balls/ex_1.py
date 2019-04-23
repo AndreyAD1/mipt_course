47daf32f3bc35e269f6053200226ff57f0ab7181
andrey.arakelyants <andrey.arakelyants@myoffice.team>
2019-04-23 10:57:14 +0300

    Try to adjust the code to many balls. But nothing has been worked still.

--- lesson_19_pygame_and_balls/ex_1.py
+++ lesson_19_pygame_and_balls/ex_1.py
@@ -1,39 +1,41 @@
 import sys
+from typing import List, Tuple, Dict
+
 import pygame
 
 
-def get_new_velocity(init_x_velocity, init_y_velocity):
+def get_new_velocity(coordinates: List) -> Tuple:
     friction_coefficient = 0.01
 
-    result_x_velocity = init_x_velocity
-    result_y_velocity = init_y_velocity
+    x_velocity, y_velocity = coordinates
 
     if pygame.key.get_pressed()[pygame.K_UP]:
-        result_y_velocity -= 5
+        y_velocity -= 5
     elif pygame.key.get_pressed()[pygame.K_DOWN]:
-        result_y_velocity += 5
+        y_velocity += 5
     elif pygame.key.get_pressed()[pygame.K_RIGHT]:
-        result_x_velocity += 5
+        x_velocity += 5
     elif pygame.key.get_pressed()[pygame.K_LEFT]:
-        result_x_velocity -= 5
+        x_velocity -= 5
     else:
         pass
 
-    result_x_velocity -= friction_coefficient * result_x_velocity
-    result_y_velocity -= friction_coefficient * result_y_velocity
+    x_velocity -= friction_coefficient * x_velocity
+    y_velocity -= friction_coefficient * y_velocity
 
-    return result_x_velocity, result_y_velocity
+    return x_velocity, y_velocity
+
+
+def check_collisions():
+    pass
 
 
 def start_ball_game(
+        balls: Dict[str, Dict[str, List]],
         display_width=500,
         display_height=500,
         display_name='Balls',
         ball_radius=20,
-        x_coord=30,
-        y_coord=30,
-        vx=50,
-        vy=50,
 ):
     screen = pygame.display.set_mode((display_width, display_height))
     pygame.display.set_caption(display_name)
@@ -46,33 +48,43 @@ def start_ball_game(
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        vx, vy = get_new_velocity(vx, vy)
+        new_ball_features = {}
+        for ball_id, ball_features in balls.items():
+            new_vx, new_vy = get_new_velocity(ball_features['velocity'])
+            new_ball_features[ball_id] = {
+                'coords': [new_vx * dt, new_vy * dt],
+                'velocity': [new_vx, new_vy]
+            }
 
-        x_coord += vx * dt
-        y_coord += vy * dt
+        balls = new_ball_features
 
-        border_reached = any(
-            [
-                x_coord < ball_radius,
-                x_coord > display_width - ball_radius,
-                y_coord < ball_radius,
-                y_coord > display_height - ball_radius
-            ]
-        )
-        if border_reached:
-            vx = -vx
-            vy = -vy
+        check_collisions()
+        # border_reached = any(
+        #     [
+        #         x_coord < ball_radius,
+        #         x_coord > display_width - ball_radius,
+        #         y_coord < ball_radius,
+        #         y_coord > display_height - ball_radius
+        #     ]
+        # )
+        # if border_reached:
+        #     vx = -vx
+        #     vy = -vy
 
         screen.fill((0, 0, 0))
-        pygame.draw.circle(
-            screen,
-            (150, 10, 50),
-            (int(x_coord), int(y_coord)),
-            20
-        )
+        for ball_features in balls.values():
+            pygame.draw.circle(
+                screen,
+                (150, 10, 50),
+                (
+                    int(ball_features['coords'][0]),
+                    int(ball_features['coords'][1])
+                ),
+                ball_radius
+            )
 
         pygame.display.flip()
 
 
 if __name__ == '__main__':
-    start_ball_game()
+    start_ball_game({'ball_1': {'coords': [30, 30], 'velocity': [50, 50]}})
