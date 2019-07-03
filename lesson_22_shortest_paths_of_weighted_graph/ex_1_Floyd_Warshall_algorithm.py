4a40fbc1dc2863f883354f46bce27f831540bf39
Arakelyants Andrey <arakelyants.andrey@yandex.ru>
2019-07-03 15:58:52 +0300

    Print interesting paths only.

--- lesson_22_shortest_paths_of_weighted_graph/ex_1_Floyd_Warshall_algorithm.py
+++ lesson_22_shortest_paths_of_weighted_graph/ex_1_Floyd_Warshall_algorithm.py
@@ -1,3 +1,4 @@
+from math import inf
 from typing import List
 
 
@@ -12,13 +13,26 @@ def get_shortest_weighted_paths(adjacency_matrix: List[List]) -> List[List]:
     :param adjacency_matrix: a list of lists representing a graph
     :return: a list of lists containing the shortest distances between vertexes.
     """
-    pass
+    for vertex_index, vertex in enumerate(adjacency_matrix):
+        for neighbour_index, neighbour in enumerate(vertex):
+            if neighbour == 0 and vertex_index != neighbour_index:
+                adjacency_matrix[vertex_index][neighbour_index] = inf
+
+    vertex_number = len(adjacency_matrix)
+    for intermediate_vertex in range(vertex_number):
+        for start_vertex in range(vertex_number):
+            for finish_vertex in range(vertex_number):
+                adjacency_matrix[start_vertex][finish_vertex] = min(
+                    adjacency_matrix[start_vertex][finish_vertex],
+                    adjacency_matrix[start_vertex][intermediate_vertex] + adjacency_matrix[intermediate_vertex][finish_vertex]
+                )
+    return adjacency_matrix
 
 
 def get_interesting_paths_only(
         shortest_paths: List[List],
-        starts: List,
-        finishes: List
+        starts: List[int],
+        finishes: List[int]
 ) -> List[List]:
     """
     Get only interesting paths from all graph paths
@@ -30,11 +44,28 @@ def get_interesting_paths_only(
     :return: a list of lists containing distances between interesting start
     and finish vertexes.
     """
-    pass
+    interesting_paths = []
+
+    for start_vertex in starts:
+        vertex_paths = []
+
+        for finish_vertex in finishes:
+            vertex_paths.append(shortest_paths[start_vertex][finish_vertex])
+
+        interesting_paths.append(vertex_paths)
+
+    return interesting_paths
 
 
 if __name__ == '__main__':
-    adjacency_matrix = [[]]
+    adjacency_matrix = [
+        [0, 1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1, 0]
+    ]
     start_vertexes = [0, 1, 2]
     finish_vertexes = [3, 4, 5]
     shortest_paths = get_shortest_weighted_paths(adjacency_matrix)
@@ -43,3 +74,4 @@ if __name__ == '__main__':
         start_vertexes,
         finish_vertexes
     )
+    print(result_matrix)
