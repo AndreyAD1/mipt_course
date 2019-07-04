10fec9b982e6e55c15ba362ab28a25a7db3a7def
Arakelyants Andrey <arakelyants.andrey@yandex.ru>
2019-07-04 22:48:51 +0300

    Get an adjacency list from input data.

--- lesson_22_shortest_paths_of_weighted_graph/ex_2_Dijkstra_algorithm.py
+++ lesson_22_shortest_paths_of_weighted_graph/ex_2_Dijkstra_algorithm.py
@@ -1,22 +1,35 @@
 from typing import Dict, List
 
 
-def get_adjacency_list(edges: List[str]) -> Dict[str, set]:
+def get_adjacency_list(edges: List[str]) -> Dict[str, Dict[str, int]] or None:
     """
     Return an adjacency list representing a graph.
 
     Convert the list of strings like 'edge_start, edge_finish, edge_weight' into
-    the adjacency list representing the graph.
+    the adjacency list representing a weighted graph.
 
     :param edges: a list of strings 'edge_start, edge_finish, edge_weight'
-    :return: a dictionary {vertex_name: neighbour_vertexe_names, ...}
+    :return: a dictionary {vertex_name: neighbour_vertex_names, ...}
     """
-    pass
+    adjacency_list = {}
+    for edge_string in edges:
+        try:
+            start_vertex, finish_vertex, weight = edge_string.split(', ')
+        except ValueError:
+            return None
+
+        if start_vertex not in adjacency_list:
+            adjacency_list[start_vertex] = {}
+
+        neighbours = adjacency_list[start_vertex]
+        neighbours[finish_vertex] = weight
+
+    return adjacency_list
 
 
 def get_shortest_distances(
         start_vertex_name: str,
-        adjacency_list: Dict[str, set]
+        adjacency_list: Dict[str, Dict[str, int]]
 ) -> Dict[str, int]:
     """
     Return a dict mapping vertex name with distance to this vertex.
@@ -25,6 +38,7 @@ def get_shortest_distances(
     :param adjacency_list: a structure representing a graph
     :return: a dict {vertex_name_1: distance_to_this_vertex, ...}
     """
+
     pass
 
 
@@ -33,13 +47,20 @@ if __name__ == '__main__':
     edge_number = 6
     edge_features = [
         '0, 1, 1',
+        '0, 5, 10',
         '1, 2, 2',
         '2, 3, 3',
-        '3, 4, 5',
-        '4, 5, 6',
-        '5, 0, 7',
+        '3, 4, 4',
+        '4, 5, 5',
+        '5, 0, 6',
+
     ]
     adjacency_list = get_adjacency_list(edge_features)
+    print(adjacency_list)
+
+    if adjacency_list is None:
+        exit('The wrong input data.')
+
     start_vertex_name = '0'
 
     if start_vertex_name not in adjacency_list:
