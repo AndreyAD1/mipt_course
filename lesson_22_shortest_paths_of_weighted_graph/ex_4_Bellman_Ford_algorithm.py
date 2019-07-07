30f75e5e5e94260ba043d0e19ca45f1fb62184bd
Arakelyants Andrey <arakelyants.andrey@yandex.ru>
2019-07-08 00:31:45 +0300

    Check the negative cycle.

--- lesson_22_shortest_paths_of_weighted_graph/ex_4_Bellman_Ford_algorithm.py
+++ lesson_22_shortest_paths_of_weighted_graph/ex_4_Bellman_Ford_algorithm.py
@@ -19,18 +19,27 @@ def get_shortest_distances(
     return distances
 
 
+def get_negative_cycle(
+        edge_list: List[Tuple[str, str, int]],
+        shortest_distances: Dict[str, int]
+) -> Tuple[str, str, int] or None:
+    for start_vertex, end_vertex, weight in edge_list:
+        if shortest_distances[start_vertex] + weight < shortest_distances[end_vertex]:
+            return start_vertex, end_vertex, weight
+
+
 if __name__ == '__main__':
     edge_features = [
         ('0', '1', 1),
-        ('0', '5', 3),
+        ('0', '5', -1),
         ('0', '2', 0),
         ('1', '2', 1),
         ('2', '3', 1),
         ('3', '4', 1),
         ('4', '5', 1),
-        ('5', '0', 1),
+        ('5', '0', -1),
     ]
-    start_vertex = '2'
+    start_vertex = '0'
     vertex_number = 6
     shortest_distances = get_shortest_distances(
         vertex_number,
@@ -38,3 +47,5 @@ if __name__ == '__main__':
         start_vertex
     )
     print(shortest_distances)
+    negative_cycle = get_negative_cycle(edge_features, shortest_distances)
+    print('Negative cycle: {}'.format(negative_cycle))
